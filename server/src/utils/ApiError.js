0c6f3581dd0da1082db2c0a1bd74339255e96214
pradeep-git-dev <narupradeep001@gmail.com>
2026-07-12 18:59:10 +0530

    Stateless Proxy & Decommissioning: Add stateless LeetCode GraphQL serverless proxy and remove legacy Express and MongoDB backend

--- server/src/utils/ApiError.js
+++
@@ -1,8 +0,0 @@
-export class ApiError extends Error {
-  constructor(statusCode, message, details = undefined) {
-    super(message);
-    this.name = "ApiError";
-    this.statusCode = statusCode;
-    this.details = details;
-  }
-}
